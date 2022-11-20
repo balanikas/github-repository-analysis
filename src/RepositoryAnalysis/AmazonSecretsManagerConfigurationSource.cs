ed20a3d6ed4e83e9a329ec3033b59409bba94c05
c_balanikas@hotmail.com <c_balanikas@hotmail.com>
2022-11-20 18:39:38 +0000

    fetch from aws secrets manager

---
+++ src/RepositoryAnalysis/AmazonSecretsManagerConfigurationSource.cs
@@ -0,0 +1,20 @@
+using Microsoft.Extensions.Configuration;
+
+namespace RepositoryAnalysis;
+
+public class AmazonSecretsManagerConfigurationSource : IConfigurationSource
+{
+    private readonly string _region;
+    private readonly string _secretName;
+
+    public AmazonSecretsManagerConfigurationSource(string region, string secretName)
+    {
+        _region = region;
+        _secretName = secretName;
+    }
+
+    public IConfigurationProvider Build(IConfigurationBuilder builder)
+    {
+        return new AmazonSecretsManagerConfigurationProvider(_region, _secretName);
+    }
+}
---
+++ src/RepositoryAnalysis/DependencyInjectionExtensions.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RepositoryAnalysis;
+
+public static class DependencyInjectionExtensions
+{
+    public static void AddAmazonSecretsManager(this IConfigurationBuilder configurationBuilder, 
+        string region,
+        string secretName)
+    {
+        var configurationSource = 
+            new AmazonSecretsManagerConfigurationSource(region, secretName);
+
+        configurationBuilder.Add(configurationSource);
+    }
+
+    public static void AddAppServices(
+        this IServiceCollection services)
+    {
+        services.AddSingleton<AnalysisService>();
+    }
+}

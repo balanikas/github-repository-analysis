f7933b96f11e5b8e951fe3d22d456b891b576028
c_balanikas@hotmail.com <c_balanikas@hotmail.com>
2022-12-01 17:59:46 +0000

    cleanup

--- src/RepositoryAnalysis/Model/Rule.cs
+++ src/RepositoryAnalysis/Model/Rule.cs
@@ -5,11 +5,11 @@ namespace RepositoryAnalysis.Model;
 public record Rule
 {
     public Guid Id { get; } = Guid.NewGuid();
-    public string Name { get; init; } = "";
+    public required string Name { get; init; }
     public string Note { get; init; } = "";
     public string? ResourceName { get; init; }
     public string? ResourceUrl { get; init; }
-    public Explanation Explanation { get; init; } = new();
+    public required Explanation Explanation { get; init; }
     public Diagnosis Diagnosis { get; init; }
     public bool ShowDetails { get; set; }
 

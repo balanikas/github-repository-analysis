3393ba3a978c4e5552230e324d2255e60772d225
c_balanikas@hotmail.com <c_balanikas@hotmail.com>
2022-12-03 16:53:55 +0000

    fix logging

--- src/Frontend/Program.cs
+++ src/Frontend/Program.cs
@@ -1,17 +1,16 @@
-using AWS.Logger.SeriLog;
 using Microsoft.AspNetCore.Hosting.StaticWebAssets;
 using MudBlazor.Services;
 using RepositoryAnalysis;
 using Serilog;
 using Serilog.Formatting.Compact;
+using Serilog.Formatting.Display;
+using Serilog.Formatting.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Logging.AddAWSProvider();
-builder.Logging.SetMinimumLevel(LogLevel.Debug);
-
 StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
 
+builder.Configuration.AddJsonFile("appsettings.json");
 builder.Host.ConfigureAppConfiguration((
     _,
     configurationBuilder) =>
@@ -19,23 +18,18 @@ builder.Host.ConfigureAppConfiguration((
     configurationBuilder.AddAmazonSecretsManager("us-west-2", "github-pat");
 });
 
+builder.Services.AddRazorPages();
+
 builder.Host.UseSerilog((
     ctx,
-    lc) =>
-{
-    lc
-        .ReadFrom.Configuration(ctx.Configuration)
-        .WriteTo.AWSSeriLog(
-            ctx.Configuration,
-            textFormatter: new RenderedCompactJsonFormatter());
-});
+    lc) =>  lc
+    .WriteTo.Console(new JsonFormatter()));
 
-builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddMudServices();
 builder.Services.AddAppServices();
-builder.Services.Configure<GitHubOptions>(
-    builder.Configuration.GetSection(GitHubOptions.GitHub));
+// builder.Services.Configure<GitHubOptions>(
+//     builder.Configuration.GetSection(GitHubOptions.GitHub));
 builder.Services.Configure<GitHubOptions>(builder.Configuration);
 
 var app = builder.Build();

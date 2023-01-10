3098ab1626fc1248368d6cad38b3617725a1f07e
balanikas <c_balanikas@hotmail.com>
2023-01-10 18:29:14 +0000

    improve dockerignore rule link

--- src/RepositoryAnalysis/Internal/Rules/Quality/DockerIgnoreRuleApplicator.cs
+++ src/RepositoryAnalysis/Internal/Rules/Quality/DockerIgnoreRuleApplicator.cs
@@ -35,7 +35,7 @@ Before the docker CLI sends the context to the docker daemon, it looks for a fil
 of the context. If this file exists, the CLI modifies the context to exclude files and directories that match patterns in it. 
 This helps to avoid unnecessarily sending large or sensitive files and directories to the daemon and potentially adding them 
 to images using ADD or COPY.",
-            AboutLink = new Link("about Dockerfile", "https://docs.docker.com/develop/develop-images/dockerfile_best-practices/")
+            AboutLink = new Link("about Dockerfile", "https://docs.docker.com/develop/develop-images/dockerfile_best-practices/#exclude-with-dockerignore")
         });
     }
 }
